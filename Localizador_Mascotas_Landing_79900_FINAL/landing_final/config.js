375c25f8e7a78ccd88db8a7bce151404d5b16688
meluriamodafemenina-boop <meluriamodafemenina@gmail.com>
2026-09-11 10:22:07 -0500

    Update config.js

--- Localizador_Mascotas_Landing_79900_FINAL/landing_final/config.js
+++ Localizador_Mascotas_Landing_79900_FINAL/landing_final/config.js
@@ -1,7 +1,7 @@
 // CONFIGURACIÓN DE SUPABASE — clave pública solamente.
 window.SUPABASE_URL = "https://girfmsczqxynfgrhhhxn.supabase.co";
 window.SUPABASE_PUBLISHABLE_KEY = "sb_publishable_UHKo5A7lWNO8SqEuMimSgg_aS7cnfG-";
-window.SUPABASE_TABLE = "pedidos";
+window.SUPABASE_TABLE = "pedidos_collares";
 
 window.supabaseClient = window.supabase.createClient(
   window.SUPABASE_URL,
